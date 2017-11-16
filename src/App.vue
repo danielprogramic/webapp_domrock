394baac158ebd25d391621f38c3878a197f07af8
danielprogramic <danielprogramic@gmail.com>
2017-11-15 23:43:23 -0200

    Redimensionando o tamanho da imagem bar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <template>
   <v-app light>
-  
-  
     <v-navigation-drawer fixed :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" class="secondary" app>
-      <v-list class="primary">
+      <v-list  class="info">
         <v-list-tile v-for="(item, i) in items" :key="i" value="true">
           <v-list-tile-action>
-            <v-icon light v-html="item.icon"></v-icon>
+            <v-icon style="color:#fff;" light v-html="item.icon"></v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            <v-list-tile-title style="color:#fff;" v-text="item.title"></v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-  
-  
     <v-toolbar dark class="primary" app>
   
       <!-- <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
@@ -29,15 +25,18 @@
               <v-icon>remove</v-icon>
             </v-btn> -->
       <v-layout row justify-space-between>
-        <v-flex xs2>
+        <v-flex xs2 style="margin-top:12px;">
           <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+           <v-btn icon @click.stop="miniVariant = !miniVariant">
+              <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+            </v-btn>
         </v-flex>
         <v-flex xs2>
           <center>
-            <img src="/static/logo-domrock-nav.gif" alt="Dom Rock" />
+            <img src="/static/logo-domrock-nav.png" alt="Dom Rock" />
           </center>
         </v-flex>
-        <v-flex xs2>
+        <v-flex xs2 style="margin-top:12px;">
           <v-spacer></v-spacer>
           <div style="float:right;">
             <v-btn icon @click.stop="rightDrawer = !rightDrawer">
@@ -46,25 +45,18 @@
           </div>
         </v-flex>
       </v-layout>
-  
-  
-  
-  
-  
-  
-  
-  
-  
     </v-toolbar>
     <v-content>
       <v-container fluid>
         <v-slide-y-transition mode="out-in">
           <v-layout column align-center>
+            </br>
+             </br>
+              </br>
             <img src="/static/logo-domrock.gif" alt="Vuetify.js" />
+                </br>
+                </br>
             <blockquote>
-  
-              </br>
-              </br>
               &#8220; “Se marcares ao largo um lampejo De um farol a mostrar o caminho, Saberá ser o nosso desejo Que jamais tu navegues sozinho”.&#8221;
               <footer>
                 <small>
@@ -72,7 +64,6 @@
                     </small>
               </footer>
             </blockquote>
-  
           </v-layout>
         </v-slide-y-transition>
       </v-container>
@@ -102,7 +93,7 @@
         drawer: false,
         fixed: false,
         items: [{
-          icon: 'bubble_chart',
+          icon: 'branding_watermark',
           title: 'Inspire'
         }],
         miniVariant: false,
